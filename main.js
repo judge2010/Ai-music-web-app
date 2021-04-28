78b68fa6c144c519dcb212c5affe63d08bf1884d
judge2010 <74088083+judge2010@users.noreply.github.com>
2021-04-28 10:46:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
-function preload(){
-
-};
-var left_wrist;
-var right_wrist;
+song = "";
+var left_wrist_x;
+var right_wrist_x;
 var right_wrist_y;
 var left_wrist_y;
+function preload(){
+
+    song=loadSound("music.mp3");
+
+};
 
 function draw(){
 image(video ,0 ,0 ,500 ,450 );
+fill("#d60909");
+stroke("#d60909");
+circle(left_wrist_x,left_wrist_y,20);
+var number_leftWristY=Number(left_wrist_y);
+var removeDecimal=floor(number_leftWristY*2);
+volume=removeDecimal/1000;
+console.log(volume);
+
+song.setVolume(volume);
 };
 function setup(){
 Canvas=createCanvas(500 ,450);
@@ -23,9 +35,14 @@ console.log("Model is loaded");
 function gotPoses(results){
     if(results.length > 0){
 console.log(results);
-left_wrist=results[0].pose.leftWrist.x;
-right_wrist=results[0].pose.rightWrist.x;
+left_wrist_x=results[0].pose.leftWrist.x;
+right_wrist_x=results[0].pose.rightWrist.x;
 left_wrist_y=results[0].pose.leftWrist.y;
 right_wrist_y=results[0].pose.rightWrist.y;
+song.play();
+    song.setVolume(0.2);
+    song.rate(1);
     };
 };
+
+    
