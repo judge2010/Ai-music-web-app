13a2b2a4171f566081ba730c7c1a630f2f07b3a6
judge2010 <74088083+judge2010@users.noreply.github.com>
2021-05-29 10:18:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 var song = "";
 var left_wrist_y;
+var left_wrist_x;
 var right_wrist_y
+var right_wrist_x;
 
 function preaload(){
-song=loadSong("poct.mp3")
+song=loadSound("poct.mp3")
 };
 function setup(){
 Canvas=createCanvas(550,500);
@@ -15,6 +17,13 @@ poseNet=ml5.poseNet(video,modelloaded);
 };
 function draw(){
     image(video, 0,0,550,500);
+    fill("#ff0000")
+    circle(left_wrist_x ,left_wrist_y ,20);
+    if(left_wrist_y < 500 && left_wrist_y > 100){
+        song.play();
+        song.setVolume(1);
+        song.rate(1);
+    };
 };
 function modelloaded(){
     console.log("Model is loaded");
@@ -24,11 +33,9 @@ function modelloaded(){
         if(results.length > 0){
     console.log(results);
     left_wrist_y=results[0].pose.leftWrist.y;
+    left_wrist_x=results[0].pose.leftWrist.x;
+    right_wrist_x=results[0].pose.rightWrist.x;
     right_wrist_y=results[0].pose.rightWrist.y;
         };      
-    if(left_wrist_y < 500 && left_wrist_y > 100){
-        song.play();
-        song.setVolume(1);
-        song.rate(1);
-    };
+    
     };
